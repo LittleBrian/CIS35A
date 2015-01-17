cf40a85c34ac7869c050c4e9b047719275005475
Brian Kring <brian.kring@arrayent.com>
2015-01-16 16:46:31 -0800

    Exercise 23
    Added Excersise 23

---
+++ Exercise23/src/bkring/Main.java
@@ -0,0 +1,38 @@
+package bkring;
+
+/*
+    Brian Kring
+    CIS35A
+    Prof. Cynthia Lee-Klawender
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        /* Variable Init */
+        int inum1 = 10;
+        int inum2 = inum1;
+
+        /* Business Logic */
+            /* Loop until iterator is equal to inum2, print iterator, decrement inum2 */
+        for ( int i=0; i < inum2; ++i, --inum2 ) {
+            System.out.println("i = " + i);
+        }
+            /* Print inum2 */
+        System.out.println("The last value of inum2 = " + inum2);
+    }
+}
+
+/* Original Code */
+
+/*
+int inum1=10, inum2;
+
+for(int i=0, inum2=inum1; i < inum2; ++i, --inum2){
+
+System.out.println("i = " + i);
+
+} // end for
+
+System.out.println("The last value of inum2 = "+inum2);
+ */
